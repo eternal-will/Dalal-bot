7dff0904be71b6c96be1de7897e1023370ade2af
ETERNAL WILL <55916653+eternal-will@users.noreply.github.com>
2021-06-12 21:32:02 +0530

    owner only commands
    limited owner commands to bot owner

--- bot.py
+++ bot.py
@@ -10,16 +10,19 @@ client=commands.Bot(command_prefix=commands.when_mentioned_or("."), intents = in
 client.remove_command("help")
 
 @client.command()
+@commands.is_owner()
 async def load(ctx, extension):
     client.load_extension(f'cogs.{extension}')
     await ctx.send(f'successfully loaded {extension}!')
 
 @client.command()
+@commands.is_owner()
 async def unload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
     await ctx.send(f'successfully unloaded {extension}!')
 
 @client.command()
+@commands.is_owner()
 async def reload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
     client.load_extension(f'cogs.{extension}')
