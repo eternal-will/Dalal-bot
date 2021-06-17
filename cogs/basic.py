93f2fa0914323aeeded224ff59b13dafacaf3197
ETERNAL WILL <55916653+eternal-will@users.noreply.github.com>
2021-06-17 09:13:37 +0530

    Update basic.py
    updated ping command

--- cogs/basic.py
+++ cogs/basic.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import time
 
 class Basic(commands.Cog):
 
@@ -12,12 +13,26 @@ class Basic(commands.Cog):
 
     @commands.command()
     async def invite(self, ctx):
-        em2 = discord.Embed(title = "Invite Link", description = "**__[Invite Link for the Bot](https://discord.com/api/oauth2/authorize?client_id=846816510306549770&permissions=2751851713&scope=bot)__**", color=16737536)
-        await ctx.send(embed = em2)
+        em3 = discord.Embed(title = "Invite Link", description = "**__[Invite Link for the Bot](https://discord.com/api/oauth2/authorize?client_id=846816510306549770&permissions=2751851713&scope=bot)__**", color=16737536)
+        await ctx.send(embed = em3)
 
     @commands.command()
     async def ping(self, ctx):
-        await ctx.send(f'Pong! {round(self.client.latency * 1000)}ms!')
+        before = time.monotonic()
+        em = discord.Embed(
+            title = "Pinging...",
+            description = "<a:CH_IconTyping:854905456551657513>",
+            color = 16737536
+        )
+        message = await ctx.send(embed=em)
+        ping = (time.monotonic() - before) * 1000
+        em2 = discord.Embed(
+            title = "Pong! :ping_pong:",
+            description = f"Client Latency: `{int(ping)}ms`",
+            color = 16737536
+        )
+        em2.set_footer(text=f"issued by {ctx.author.name}")
+        await message.edit(embed = em2)
 
 def setup(client):
     client.add_cog(Basic(client))
