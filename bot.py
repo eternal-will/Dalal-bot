4f7b9858f523e9f5a902b41ce5ef67117f994de2
ETERNAL WILL <55916653+eternal-will@users.noreply.github.com>
2021-06-17 22:09:13 +0530

    Update bot.py
    removed useless restart command

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 # bot.py
 import os
-import sys
 import discord
 from discord.ext import commands
 
@@ -41,14 +40,5 @@ async def reload(ctx, extension):
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
-
-def restart_bot(): 
-  os.execv(sys.executable, ['python'] + sys.argv)
-
-@client.command(hidden=True)
-@commands.is_owner()
-async def restart(ctx):
-  await ctx.send("Restarting bot...")
-  restart_bot()
 
 client.run(TOKEN)
