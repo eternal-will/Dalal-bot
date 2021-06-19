db4b282fba354d4241a47d123dd12b851c9a3a7d
ETERNAL WILL <55916653+eternal-will@users.noreply.github.com>
2021-06-19 15:52:22 +0530

    moved prefix to .env
    and some minor changes

--- bot.py
+++ bot.py
@@ -6,22 +6,24 @@ from discord.ext import commands
 
 load_dotenv('.env')
 
+prefix = os.getenv('PREFIX')
+
 intents = discord.Intents.all()
-client=commands.Bot(command_prefix=commands.when_mentioned_or("."), intents = intents)
+client=commands.Bot(command_prefix=commands.when_mentioned_or(f"{prefix}"), intents = intents)
 
 @client.command(hidden = True)
 @commands.is_owner()
 async def load(ctx, extension):
     client.load_extension(f'cogs.{extension}')
     print(f'successfully loaded {extension}!')
-    await ctx.send(f'successfully loaded {extension}!')
+    await ctx.send(f'successfully loaded `{extension}`!')
 
 @client.command(hidden = True)
 @commands.is_owner()
 async def unload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
     print(f'successfully unloaded {extension}!')
-    await ctx.send(f'successfully unloaded {extension}!')
+    await ctx.send(f'successfully unloaded `{extension}`!')
 
 @client.command(hidden = True)
 @commands.is_owner()
@@ -36,7 +38,7 @@ async def reload(ctx, extension):
         client.unload_extension(f'cogs.{extension}')
         client.load_extension(f'cogs.{extension}')
         print(f'successfully reloaded {extension}!')
-        await ctx.send(f'successfully reloaded {extension}!')
+        await ctx.send(f'successfully reloaded `{extension}`!')
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
