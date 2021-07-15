0db48f48326aad58df6b81bc43c35118e7405cce
ETERNAL WILL <55916653+eternal-will@users.noreply.github.com>
2021-07-15 13:35:50 +0530

    tried adding realtime logging thingy

--- starter.py
+++ starter.py
@@ -3,10 +3,11 @@ import discord
 from discord.ext import commands
 import subprocess
 from dotenv import load_dotenv
+import asyncio
 
 load_dotenv('.env')
-
-client=commands.Bot(command_prefix='!@')
+intents = discord.Intents.all()
+client=commands.Bot(command_prefix='@!', intents = intents)
 
 @client.event
 async def on_command_error(error):
@@ -15,16 +16,22 @@ async def on_command_error(error):
     else:
         pass
 
-@client.command(name='start_bot', aliases=['sb'])
-@commands.is_owner()
-async def start_bot(ctx):
-    dalal = subprocess.Popen(os.getenv('BOT_STARTSH_PATH'), shell=True)
-    await ctx.reply('turning bot on...', mention_author=False)
-
-@client.command(name='git_pull', aliases = ['gp'])
+@client.command(name='git_pull', aliases = ['gp'], hidden=True)
 @commands.is_owner()
 async def git_pull(ctx):
     git_pull_repo = subprocess.Popen(os.getenv('GIT_PULL_SH_PATH'), shell=True)
     await ctx.reply('git pull initiated...', mention_author=False)
 
-client.run(os.getenv('TOKEN'))
+@client.command(name='start_bot', aliases=['sb'], hidden=True)
+async def start_bot(ctx):
+    process = await asyncio.create_subprocess_exec(
+        'python3', 
+        os.getenv('BOT_PY_PATH'), 
+        stdout=subprocess.PIPE)
+    stdout, stderr = await process.communicate()
+    logs = stdout.decode().strip()
+    await ctx.reply('turning bot on...', mention_author=False)
+    channel = client.get_channel(865139254324494346)
+    await channel.send(logs)
+
+client.run('ODU1Mzg4NTc1Njg0MzYyMjQw.YMxwsA.TipSnQSqYF0DudiOyblorA4nzAs')
