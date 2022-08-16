b4d3c28c6164faa72b5f3a93df5d926c904e1f49
ETERNAL WILL <55916653+eternal-will@users.noreply.github.com>
2022-08-17 00:40:09 +0530

    removed malenude & rnsfw command

--- cogs/nsfwsub.py
+++ cogs/nsfwsub.py
@@ -54,18 +54,8 @@ class NSFWSub(commands.Cog, name='NSFW_Commands'):
         await self.run_comm(ctx, subreddit_name)
 
     @commands.command(name = "nsfw", description = f"**Command format:** `.nsfw <subreddit name>`\n• Provides an nsfw post from the mentioned subreddit.\n• __r/holdthemoan__ is default and is used if no subreddit is provided.\n• Can only be used in a [channel marked as nsfw](https://support.discord.com/hc/en-us/articles/115000084051-NSFW-Channels-and-Content)")
-    async def nsfw(self, ctx, subreddit_name = f"holdthemoan"):
-        if subreddit_name == 'sshashwat' or subreddit_name == 'shswt' or subreddit_name == 'shashwat' or subreddit_name == '_sshashwat' or subreddit_name == 'susuwant'or subreddit_name == '_shashwat':
-            url1 = 'https://i.imgur.com/OpRMyR5.jpg'
-            msg = f"Looking for **Shashwat's** nudes?\n`Rather have some Jawline pics` <a:awink_thumbsup:855303753011691520>"
-            em2 = Embed(
-                title = "Shashwat's Noods :hot_face:",
-                description = msg,
-                img_url=url1
-            )
-            await ctx.reply(embed = em2, mention_author=False)
-        else:
-            await self.run_comm(ctx, subreddit_name)
+    async def nsfw(self, ctx, subreddit_name = f"holdthemoan+nsfw"):
+        await self.run_comm(ctx, subreddit_name)
 
     @nsfw.error
     async def nsfw_error(self, ctx, error):
@@ -73,19 +63,9 @@ class NSFWSub(commands.Cog, name='NSFW_Commands'):
             await ctx.reply('Failed to find such subreddit.', mention_author=False)
             raise error
 
-    @commands.command(name = "rnsfw", description = "• Shows an nsfw post from __r/nsfw__.\n• Can only be used in a [channel marked as nsfw](https://support.discord.com/hc/en-us/articles/115000084051-NSFW-Channels-and-Content)")
-    async def rnsfw(self, ctx):
-        subreddit_name='nsfw'
-        await self.run_comm(ctx, subreddit_name)
-
     @commands.command(name = "hentai", description = "• Shows an nsfw post from __r/hentai__.\n• Can only be used in a [channel marked as nsfw](https://support.discord.com/hc/en-us/articles/115000084051-NSFW-Channels-and-Content)")
     async def hentai(self, ctx):
         subreddit_name='hentai'
-        await self.run_comm(ctx, subreddit_name)
-
-    @commands.command(name= "malenudes", aliases = ['nudemale', 'nudemales', 'malenude', 'nakedmales', 'nakedmale'], description = "• Why shud boys have all the fun? <a:awink_thumbsup:855303753011691520>\n• Displays a post containing **__Male Nudes__**\n• Can only be used in a [channel marked as nsfw](https://support.discord.com/hc/en-us/articles/115000084051-NSFW-Channels-and-Content)")
-    async def malenudes(self, ctx):
-        subreddit_name = choice(redd.MN_SUB)
         await self.run_comm(ctx, subreddit_name)
 
     @commands.command(name="ass", aliases = ['butt', 'booty'], description = "• Command for booty lovers :peach:. \n• Fetches a post containing ass.\n• Can only be used in a [channel marked as nsfw](https://support.discord.com/hc/en-us/articles/115000084051-NSFW-Channels-and-Content)")
@@ -98,7 +78,7 @@ class NSFWSub(commands.Cog, name='NSFW_Commands'):
         subreddit_name = choice(redd.PUSSY_SUB)
         await self.run_comm(ctx, subreddit_name)
 
-    @commands.command(name='bdsm', aliases=['kink', 'kinky'], description="• Command for BDSM lovers :<:hunter:861866842065993778>:.\n• Can only be used in a [channel marked as nsfw](https://support.discord.com/hc/en-us/articles/115000084051-NSFW-Channels-and-Content)")
+    @commands.command(name='bdsm', aliases=['kink', 'kinky'], description="• Command for BDSM lovers <:hunter:861866842065993778>.\n• Can only be used in a [channel marked as nsfw](https://support.discord.com/hc/en-us/articles/115000084051-NSFW-Channels-and-Content)")
     async def bdsm(self, ctx):
         subreddit_name = choice(redd.BDSM_SUB)
         await self.run_comm(ctx, subreddit_name)
